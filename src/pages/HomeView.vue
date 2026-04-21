bdb1633994b388e36651aab2e80f9fbdcce19a71
yanluoluo <1799191542@qq.com>
2026-04-21 19:29:42 +0800

    feat: 修改首页顶部topbar

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -3,12 +3,19 @@
     <div class="flex flex-row w-full h-12 shadow-2xl shadow-dark-400">
       <!-- 其他按钮 -->
       <div class="flex flex-row">
-        <TabBarButton :activeClassName="route.path === '/' ? 'text-blue' : ''">首页</TabBarButton>
-        <TabBarButton :activeClassName="route.path === '/diary' ? 'text-blue' : ''">记录</TabBarButton>
+        <TabBarButton :activeClassName="route.path === '/' ? 'text-blue' : ''">
+          <router-link to="/">首页</router-link>
+        </TabBarButton>
+        <TabBarButton :activeClassName="route.path === '/diary' ? 'text-blue' : ''">
+          <router-link to="/diary">日志</router-link>
+        </TabBarButton>
         <TabBarButton :activeClassName="route.path === '/gameSelector' ? 'text-blue' : ''">
-          <router-link to="/gameSelector">小游戏</router-link>
+          <router-link to="/gameSelector">游戏Demo</router-link>
         </TabBarButton>
       </div>
+    </div>
+    <div class="flex flex-col h-full mt-4 overflow-y-auto">
+      <router-view />
     </div>
   </div>
 </template>
